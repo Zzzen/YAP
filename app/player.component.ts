963c370e5d1ff21ea57771c84f893cc57e6fe72f
Zen <843968788@qq.com>
2016-06-19 22:35:48 +0800

    rewrite ui

--- app/player.component.ts
+++ app/player.component.ts
@@ -1,21 +1,26 @@
 import {Component, ElementRef, OnInit, OnDestroy, Input} from "@angular/core";
 import {remote} from "electron";
 import $ = require("jquery");
+import _ = require("lodash");
 
 import {VideoService} from "./video.service";
 import {PreferenceService} from "./preference.service";
 import {Video} from "./models";
+
+const renderer = require("wcjs-renderer");
+const vlc = require("wcjs-prebuilt").createPlayer();
+
+// milliseconds
+const DBCLICK_INTERVAL = 360;
+
 // import {stat} from "./promisifiedNode";
 
-const wcjs: any = require("wcjs-player");
 
 // const SUPPORTED_SUBTITLE_FORMAT = ["sub", "srt", "webvtt"];
 
 @Component({
     selector: "yap-player",
-    template: `
-        <div id="player" [class.isHidden]="focusSetting" > </div>
-    `
+    templateUrl: "./player.component.html"
 })
 export class PlayerComponent implements OnInit, OnDestroy {
     // rawPlayer: HTMLVideoElement;
@@ -25,9 +30,29 @@ export class PlayerComponent implements OnInit, OnDestroy {
 
     subscription: any;
 
-    player: any;
-
     private currentVideo: Video;
+
+    private currentVideoLength = 0;
+
+    private currentShowedTime = 0;
+
+    private vlcTime = 0;
+
+    private isDraggingTime = false;
+
+    private isPlaying = false;
+
+    private showControls = true;
+    private moveTimerId: number = undefined;
+
+    private canvas: HTMLCanvasElement;
+
+    private clickTimerId: number = undefined;
+
+    static toggleFullscreen() {
+        const isFullScreen = remote.getCurrentWindow().isFullScreen();
+        remote.getCurrentWindow().setFullScreen(!isFullScreen);
+    }
 
     constructor(
         private element: ElementRef,
@@ -48,23 +73,25 @@ export class PlayerComponent implements OnInit, OnDestroy {
                     break;
 
                 case " ":
-                    this.player.togglePause();
+                    this.togglePause();
                     break;
 
                 case "ArrowLeft":
-                    this.player.time(this.player.time() - 5000);
+                    this.currentShowedTime -= 5000;
+                    vlc.time -= 5000;
                     break;
 
                 case "ArrowRight":
-                    this.player.time(this.player.time() + 5000);
+                    this.currentShowedTime += 5000;
+                    vlc.time += 5000;
                     break;
 
                 case "ArrowUp":
-                    this.player.volume(this.player.volume() + 10);
+                    vlc.volume += 10;
                     break;
 
                 case "ArrowDown":
-                    this.player.volume(this.player.volume() - 10);
+                    vlc.volume -= 10;
                     break;
 
                 default:
@@ -79,83 +106,185 @@ export class PlayerComponent implements OnInit, OnDestroy {
         }, true);
     }
 
+    togglePause() {
+        vlc.togglePause();
+        this.isPlaying = !this.isPlaying;
+    }
+
+    onClick() {
+        if (undefined !== this.clickTimerId) {
+            window.clearTimeout(this.clickTimerId);
+            this.clickTimerId = undefined;
+
+            // toggleFullscreen
+            const isFullScreen = remote.getCurrentWindow().isFullScreen();
+            remote.getCurrentWindow().setFullScreen(!isFullScreen);
+
+        } else {
+            this.clickTimerId = window.setTimeout(() => {
+                if (this.currentVideo) {
+                    this.togglePause();
+                }
+                this.clickTimerId = undefined;
+            }, DBCLICK_INTERVAL);
+        }
+    }
+
+
     ngOnInit() {
+        this.canvas = document.getElementById("canvas") as HTMLCanvasElement;
+
+        renderer.bind(this.canvas, vlc);
+
+        document.addEventListener("mousemove", event => {
+            if (0 !== event.movementX && 0 !== event.movementY) {
+                this.showControls = true;
+            }
+
+            if (this.moveTimerId !== undefined) {
+                window.clearTimeout(this.moveTimerId);
+            }
+
+            this.moveTimerId = window.setTimeout(() => {
+                this.moveTimerId = undefined;
+                this.showControls = false;
+            }, 3000);
+        });
+
         this.initKeyboardListener();
 
         this.initPlayer();
 
-        this.player.onEnded(() => {
-            this.currentVideo.position = 0;
-
-            const next = this.videoService.getNearbyVideos(this.currentVideo).next;
-            if (next) { this.playVideo(next); }
-        });
-
-        this.player.onTime((time: number) => this.onTimeUpdate(time));
-
         this.subscription = this.videoService.playingChange.subscribe((video: Video) => { this.playVideo(video); });
     }
 
+    getTransform() {
+        // todo: optimization
+        const parent = this.canvas.parentElement;
+
+        const parentHeight = $(parent).height();
+        const parentWidth = $(parent).width();
+
+        const canvasHeight = $(this.canvas).height();
+        const canvasWidth = $(this.canvas).width();
+
+
+        const ratio = Math.min(parentWidth / canvasWidth,
+            parentHeight / canvasHeight);
+
+        return `translate(-50%, -50%) scale(${ratio})`;
+    }
+
+    setVolume(volume: number) {
+        vlc.volume = 200 < volume ? 200 : volume;
+        this.preferenceService.preference.volume = vlc.volume;
+    }
+
+    onWheel($event: WheelEvent) {
+        this.setVolume(vlc.volume + 10 * ($event.deltaY > 0 ? -1 : 1));
+    }
+
+
+    logX(s: string) {
+        console.log(s);
+    }
+
+    onTimeChangedManually($event: Event) {
+        // console.log("drag ended");
+
+        // tofix: this may be buggy?
+        // since `startDraggingTime($event)` is triggered by focus
+        ($event.target as HTMLInputElement).blur();
+
+        this.isDraggingTime = false;
+
+        // vlc.time = parseInt(($event.target as HTMLInputElement).value, 10);
+    }
+
+    onTimeClick($event: Event) {
+        // console.log("click end");
+        this.isDraggingTime = false;
+        vlc.time = parseInt(($event.target as HTMLInputElement).value, 10);
+    }
+
+    startDraggingTime($event: Event) {
+        // console.log("drag start");
+        this.isDraggingTime = true;
+    }
+
+    stopPropagation($event: Event) {
+        $event.stopPropagation();
+    }
+
     private initPlayer() {
-        this.player = new wcjs("#player").addPlayer({
-            autoplay: true,
-            wcjs: require("wcjs-prebuilt")
-        });
+        // todo: find a better way
+        window.setInterval(() => {
+            this.canvas.style.transform = this.getTransform();
+        }, 100);
 
-        $(".wcp-subtitle-but").hide();
+        vlc.onEndReached = () => {
+            this.currentVideo.position = 0;
 
-        const $wcpSurface = $(".wcp-surface");
-        $wcpSurface[0].addEventListener("wheel", event => {
-            const before: number = this.player.volume();
-            const after: number = before + 10 * (event.deltaY > 0 ? -1 : 1);
+            const next = this.videoService.getNearbyVideos(this.currentVideo).next;
+            if (next) {
+                this.playVideo(next);
+            } else {
+                // todo: fix?
+                this.currentVideo = undefined;
 
-            this.player.volume(after);
-        });
+                // vlc.time = 0;
+                // vlc.play();
+                // vlc.pause();
+            }
+        };
+
+        vlc.onTimeChanged = (time: number) => {
+            this.vlcTime = time;
+
+            this.currentVideo.position = time;
+
+            if (!this.isDraggingTime) {
+                this.currentShowedTime = time;
+            }
+        };
+
+        vlc.onLengthChanged = (length: number) => {
+            this.currentVideoLength = length;
+        };
+
 
         this.preferenceService.updateEmitter.subscribe(() => {
-            this.player.volume(this.preferenceService.preference.volume);
+            vlc.volume = this.preferenceService.preference.volume;
         });
 
-        this.player.onVolume((newValue: number) => {
-            this.preferenceService.preference.volume = newValue;
-        });
+        // function hideTitle() { $(".wcp-titlebar").fadeOut("slow"); }
 
-        function toggleFullscreen() {
-            const isFullScreen = remote.getCurrentWindow().isFullScreen();
-            remote.getCurrentWindow().setFullScreen(!isFullScreen);
-            if (isFullScreen) {
-                $(".wcp-minimize").removeClass("wcp-minimize").addClass("wcp-maximize");
-            } else {
-                $(".wcp-maximize").removeClass("wcp-maximize").addClass("wcp-minimize");
-            }
+        // let timerId = window.setTimeout(hideTitle, 3000);
+        // $wcpSurface.unbind("mousemove");
+        // const $titleBar = $(".wcp-titlebar");
+        // $wcpSurface.mousemove(event => {
+        //     window.clearTimeout(timerId);
+        //     timerId = window.setTimeout(hideTitle, 3000);
+
+        //     if (!$titleBar.is(":visible")) {
+        //         $titleBar.fadeIn("slow");
+        //     }
+        // });
+
+    }
+
+    tryPlayingPrev() {
+        const prev = this.videoService.getNearbyVideos(this.currentVideo).prev;
+        if (prev) {
+            this.playVideo(prev);
         }
+    }
 
-        $(".wcp-button.wcp-right.wcp-maximize").unbind("click");
-        $(".wcp-button.wcp-right.wcp-maximize").click(() => toggleFullscreen());
-
-        // toggle fullscreen when dblclick
-        $wcpSurface.unbind("dblclick");
-        $wcpSurface.dblclick(event => {
-            $(".wcp-anim-basic").finish();
-            $(".wcp-pause-anim").finish();
-
-            toggleFullscreen();
-        });
-
-        function hideTitle() { $(".wcp-titlebar").fadeOut("slow"); }
-
-        let timerId = window.setTimeout(hideTitle, 3000);
-        $wcpSurface.unbind("mousemove");
-        const $titleBar = $(".wcp-titlebar");
-        $wcpSurface.mousemove(event => {
-            window.clearTimeout(timerId);
-            timerId = window.setTimeout(hideTitle, 3000);
-
-            if (!$titleBar.is(":visible")) {
-                $titleBar.fadeIn("slow");
-            }
-        });
-
+    tryPlayingNext() {
+        const next = this.videoService.getNearbyVideos(this.currentVideo).next;
+        if (next) {
+            this.playVideo(next);
+        }
     }
 
     playVideo(video: Video) {
@@ -163,9 +292,11 @@ export class PlayerComponent implements OnInit, OnDestroy {
             return;
         } else {
             this.currentVideo = video;
+            this.isPlaying = true;
 
-            this.player.vlc.play(video.fullpath);
-            this.player.time(video.position);
+            vlc.play(video.fullpath);
+            vlc.time = video.position;
+
 
             // todo: support subtitles?
 
@@ -212,9 +343,14 @@ export class PlayerComponent implements OnInit, OnDestroy {
         this.subscription.unsubscribe();
     }
 
-    onTimeUpdate(milliseconds: number) {
-        if (this.currentVideo) {
-            this.currentVideo.position = milliseconds;
-        }
+    formatTime(milliseconds: number) {
+        const seconds = milliseconds / 1000;
+
+        const hours = _.padStart(Math.floor(seconds / 3600) + "", 2, "0");
+        const hourReminder = seconds % 3600;
+        const minutes = _.padStart(Math.floor(hourReminder / 60) + "", 2, "0");
+        const minuteReminder = _.padStart(Math.floor(hourReminder % 60) + "", 2, "0");
+
+        return `${hours}:${minutes}:${minuteReminder}`;
     }
 }
